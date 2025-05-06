8c59e149638e9d2194a2746afa3589a35b5596a3
PerseveranceGu <ahmed.lehanine@gmail.com>
2025-05-06 22:06:26 +0100

    m

--- lib/Screens/delete_profile_page.dart
+++ lib/Screens/delete_profile_page.dart
@@ -144,7 +144,7 @@ class _DeleteProfilePageState extends State<DeleteProfilePage>
       },
       "minigames": {
         "Find": [false, false, false, false],
-        "Puzzle": [false, false, false, true],
+        "Puzzle": [false, false, false, false],
         "Match": [false, false, false, false],
         "Choose": [false, false, false, false],
         "Memory": [false, false, false, false],
